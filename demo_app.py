2a328ae67f4edfdaed3d192f741c635f772896fa
varvarafurik <vfurik@nes.ru>
2021-04-11 20:35:44 +0300

    Initial commit

--- demo_app.py
+++ demo_app.py
@@ -13,15 +13,13 @@ with st.echo(code_location='below'):
     gender = pd.read_csv("1310038301_gender.csv")
     age = pd.read_csv("1310038301_age.csv")
     df.drop(columns = ['DGUID','SCALAR_FACTOR','SCALAR_ID','VECTOR','COORDINATE','STATUS','TERMINATED','DECIMALS'])
-    pd.set_option('display.max_columns', None)
-    pd.set_option('display.max_rows', None)
 
     #BLOCK1
 
     mean_ter = df[(df['Self-reported cannabis use in the past three months'] == "Percentage of people")]
     mean_ter = mean_ter.groupby("GEO").mean()
     mean_ter.reset_index(level=0, inplace=True)
-    sns.set(rc={'figure.figsize':(25.7,14.27)})
+    sns.set(rc={'figure.figsize':(30.7,14.27)})
     order=['Canada', 'Alberta', 'British Columbia','Iqaluit (Nunavut)', 'Manitoba', 'New Brunswick', 'Newfoundland and Labrador', 'Nova Scotia', 'Ontario',
     'Prince Edward Island', 'Quebec', 'Saskatchewan', 'Whitehorse (Yukon)',  'Yellowknife (Northwest Territories)']
     Can_per = (float(mean_ter[mean_ter["GEO"] == "Canada"]["VALUE"]))
@@ -29,24 +27,22 @@ with st.echo(code_location='below'):
     #ax1 = sns.set_style(style=None, rc=None )
     fig1, ax1 = plt.subplots(figsize=(30.7,14.27))
     sns.barplot(x='VALUE', y="GEO", data=mean_ter, orient='h', order=order, ax=ax1)
-    ax1.set_title("Average percentage of users in Canada, and province, territory", fontdict= { 'fontsize': 24, 'fontweight':'bold'})
-    plt.xlabel("Percentage of users")
-    plt.ylabel("Province or territory")
+    ax1.set_title("Average percentage of users in Canada, provinces, and territories", fontdict= {'fontsize': 26})
+    plt.xlabel("Percentage of users", fontdict= {'fontsize': 26})
+    plt.ylabel("Province or territory", fontdict= {'fontsize': 26})
     ax2 = ax1.twinx()
-    #mark=[[1, 5, 0], [1,5,25], [2, 5, 0], [2,5,25], [3, 5, 0], [3,5,25]]
-    #marks = pd.DataFrame(mark, columns=['number', "perc1", "perc2"], dtype=float)
     sns.lineplot(x=[Can_per-5, Can_per-5], y=[0,30.7], ax=ax2, markers=True, color="blue")
     ax3 = ax1.twinx()
     sns.lineplot(x=[Can_per+5, Can_per+5], y=[0, 30.7], ax=ax3, markers=True, color="blue")
-    ax3.text(25.85, 28.85, '8 out of 13 territories are in 5% interval', fontsize=12)
-    ax3.text(25.85, 29.85, 'Red line indicates the average in the country', fontsize=12)
-    plt.axvline(Can_per, color='red')
+    ax3.text(25.85, 28.85, '8 out of 13 territories are in 5% interval', fontsize=14)
+    ax3.text(25.85, 29.85, 'Red line indicates the average in the country.', fontsize=14)
+    plt.axvline(Can_per, color='red', linestyle="--")
     st.pyplot(fig1)
 
     #BLOCK2
 
     quat_ter = df[(df['Self-reported cannabis use in the past three months'] == "Number of people") & (df["GEO"] != "Canada") & (df["GEO"] != 'Iqaluit (Nunavut)') & (df["GEO"] != 'Yellowknife (Northwest Territories)') & (df["GEO"] != 'Whitehorse (Yukon)')]
-    fig2 = px.scatter(quat_ter, x="GEO", y="VALUE", hover_data=["REF_DATE"], color="GEO", labels={'GEO': "Province or territory", 'REF_DATE':"Quarter", "VALUE": "Number of users" })
+    fig2 = px.scatter(quat_ter, x="GEO", y="VALUE", hover_data=["REF_DATE"], color="GEO", labels={'GEO': "Province or territory", 'REF_DATE':"Quarter", "VALUE": "Number of users" }, title="Number of users in provinces or territories, quarterly",)
     st.plotly_chart(fig2)
 
     #BLOCK3
@@ -59,19 +55,21 @@ with st.echo(code_location='below'):
     plt.figlegend(bbox_to_anchor=(0.9, 0.9), loc='upper left')
     plt.xlabel("Quarter", size=12)
     plt.ylabel("Percentage of users", size=12)
-    plt.title("Percentage of users per quarter in each province or territory", size = 16)
+    plt.title("Percentage of users per quarter in province or territory", size = 16)
+    plt.text(2.05,33,"Nova Scotia, 32.8%")
+    plt.text(0.75, 11.25, "Quebec, 10.3%")
     st.pyplot(fig3)
 
     #BLOCK4
 
-    fig4, ax4 = plt.subplots()
     gen_dist = gender[gender['Self-reported cannabis use in the past three months'] == "Number of people"]
-   #gen_dist = gen_dist.groupby("GEO").mean()
-    gen_dist.reset_index(level=0, inplace=True)
-    sns.violinplot(y="VALUE", x="GEO", data=gen_dist, palette="Set2", split=True,
-                    scale="count", inner="stick", estimator="mean",
-                    scale_hue=False, bw=.2, ax=ax4, labels={'GEO': "Sex of users", "VALUE": "Number of users"}, title="Number of users for people of both sexex, quarterly")
-    st.pyplot(fig4)
+    gen_perc = gender[gender['Self-reported cannabis use in the past three months'] == "Percentage of people"]
+    gen_perc=gen_perc.reset_index()
+    gen_perc["NUM"] = gender[gender['Self-reported cannabis use in the past three months'] == "Number of people"]["VALUE"].values
+    fig4=px.scatter(gen_perc, x="REF_DATE", y="VALUE", animation_frame="REF_DATE", animation_group="GEO",
+                    size='NUM', color="GEO", hover_name="GEO", range_x=["2019-01", "2019-12"], range_y=[0, 40], title="Percentage of users among both sexes, quarterly",
+                     labels={'REF_DATE':"Quarter", "VALUE": "Percentage of users", "NUM":"Number of users", "GEO": "Sex"})
+    st.plotly_chart(fig4)
 
     #BLOCK5
 
@@ -89,21 +87,36 @@ with st.echo(code_location='below'):
                 gen_dist["GEO"] == "Canada, males") & (gen_dist["UOM"] == "Number")]["VALUE"]) / q4]
     labels="Female", "Male"
     colors=['#ff9999','#66b3ff']
-    ax5[0, 0].pie(s1, labels=labels, normalize=True, radius=0.8, textprops={'fontsize': 8}, autopct='%1.1f%%', colors=colors)
-    ax5[0, 0].set_title("Quarter 1", loc="center", fontsize="small")
-    ax5[0, 1].pie(s2, labels=labels, normalize=True, radius=0.8, textprops={'fontsize': 8}, autopct='%1.1f%%', colors=colors)
-    ax5[0, 1].set_title("Quarter 2", loc="center", fontsize="small")
-    ax5[1, 0].pie(s3, labels=labels, normalize=True, radius=0.8, textprops={'fontsize': 8}, autopct='%1.1f%%', colors=colors)
-    ax5[1, 0].set_title("Quarter 3", loc="center", fontsize="small")
-    ax5[1, 1].pie(s4, labels=labels, normalize=True, radius=0.8, textprops={'fontsize': 8}, autopct='%1.1f%%', colors=colors)
-    ax5[1, 1].set_title("Quarter 4", loc="center", fontsize="small")
+    ax5[0, 0].pie(s1, labels=labels, normalize=True, radius=0.7, textprops={'fontsize': 8}, autopct='%1.1f%%', colors=colors)
+    ax5[0, 0].set_title("Quarter 1", loc="center", fontsize="small", y=0.9)
+    ax5[0, 1].pie(s2, labels=labels, normalize=True, radius=0.7, textprops={'fontsize': 8}, autopct='%1.1f%%', colors=colors)
+    ax5[0, 1].set_title("Quarter 2", loc="center", fontsize="small", y=0.9)
+    ax5[1, 0].pie(s3, labels=labels, normalize=True, radius=0.7, textprops={'fontsize': 8}, autopct='%1.1f%%', colors=colors)
+    ax5[1, 0].set_title("Quarter 3", loc="center", fontsize="small", y=0.9)
+    ax5[1, 1].pie(s4, labels=labels, normalize=True, radius=0.7, textprops={'fontsize': 8}, autopct='%1.1f%%', colors=colors)
+    ax5[1, 1].set_title("Quarter 4", loc="center", fontsize="small", y=0.9)
+    fig5.suptitle("Distribution of users by sex, quarterly")
     st.pyplot(fig5)
 
     #BLOCK6
     age_dist = age[age['Self-reported cannabis use in the past three months'] == "Percentage of people"]
     age_dist=age_dist.reset_index()
     age_dist["NUM"] = age[age['Self-reported cannabis use in the past three months'] == "Number of people"]["VALUE"].values
-    print(age_dist)
-    fig6=px.scatter(age_dist, x="REF_DATE", y="VALUE", animation_frame="REF_DATE", animation_group="GEO",
-                    size='NUM', color="GEO", hover_name="GEO", range_x=["2019-01", "2019-12"], range_y=[0, 40])
+    fig6 = (px.scatter(age_dist, x="REF_DATE", y="VALUE", animation_frame="REF_DATE", animation_group="GEO",
+                    size='NUM', color="GEO", hover_name="GEO", range_x=["2019-01", "2019-12"], range_y=[0, 40],
+                    title="Percentage of users among different age groups, quarterly",
+                    labels={'REF_DATE': "Quarter", "VALUE": "Percentage of users"}))
+
+    age_dist = age[age['Self-reported cannabis use in the past three months'] == "Percentage of people"]
+    m6 = age_dist.groupby("REF_DATE").mean().reset_index()["VALUE"].mean()
+    fig6.add_hline(float(m6), line_width=1, fillcolor="red", opacity=0.2, annotation_text="Average throughout the year", annotation_position="bottom right")
     st.plotly_chart(fig6)
+
+    #BLOCK7
+    age_dist = (age[age['Self-reported cannabis use in the past three months'] == "Number of people"]).rename(columns={"GEO":"Age group"})
+    fig7=sns.relplot(x="REF_DATE", y="VALUE", hue="Age group",
+                col="Age group", height=4, kind="line", estimator=None, data=age_dist, linewidth=2.5, col_wrap=3)
+    #fig7.set_titles("Number of users among different age groups, quarterly")
+    fig7.set_axis_labels("Quarter", "Number")
+    fig7.fig.suptitle("Number of users among different age groups, quarterly",  x=0.5, y=1.05)
+    st.pyplot(fig7)
